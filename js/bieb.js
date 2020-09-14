ae355289ae52219031b784b6557b2fce3a4a2371
plantenbieb <plantenbieb@outlook.com>
2020-09-14 11:13:35 +0200

    map alpha 0.2

---
+++ js/bieb.js
@@ -0,0 +1,53 @@
+storeLocator({
+    container: 'my-store-locator',
+    apiKey: 'AIzaSyA55LXw2hfXdWtViSc28ncD2MXjToagmJw',
+    markerIconSize: [40, 40],
+    limit: 1,
+    center: {lat: 51.9074, lng: 4.4930},
+    zoom: 11,
+    searchHint: "Kies een plantenbieb uit!",
+    homeLocationHint: 'Current location',
+    stores: [
+      {
+        name: 'Plantenbieb.com',
+        address: 'Meld je plantenbieb nu aan!',
+        location: {lat: 51.9074, lng: 4.4930},
+        website: 'https://plantenbieb.com'
+      },
+      {
+        name: 'NAAM',
+        address: "BESCHRIJVING",
+        location: {lat: 51.909101, lng: 4.446417}
+      },
+      {
+        name: "NAAM",
+        address: 'BESCHRIJVING',
+        location: {lat: 51.914326, lng: 4.400137}
+      },
+      {
+        name: 'NAAM',
+        address: 'BESCHRIJVING',
+        location: {lat: 51.908885, lng: 4.350489}
+      },
+      {
+        name: 'PAEZ PALMA',
+        address: 'Carrer de la Bosseria, 12, 07001 Palma, Illes Balears, Spain',
+        location: {lat: 39.5706846, lng: 2.651996899999972}
+      },
+      {
+        name: 'PAEZ TENERIFE',
+        address: 'Calle Ramón y Cajal, 37, 38004 Santa Cruz de Tenerife, Spain',
+        location: {lat: 28.4660137, lng: -16.258979700000054}
+      },
+      {
+        name: 'PAEZ CHIADO',
+        address: 'R. do Alecrim 62, 1200-243 Lisboa, Portugal',
+        location: {lat: 38.7094783, lng: -9.143068699999958}
+      },
+      {
+        name: 'PAEZ PORTO',
+        address: 'R. das Carmelitas 151, 4050-162 Porto, Portugal',
+        location: {lat: 41.1462871, lng: -8.61489800000004}
+      }
+    ]
+  })
