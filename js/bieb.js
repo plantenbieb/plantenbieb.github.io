209c6cb192e83c9a0924b72d1fdbf36fc0e8b000
plantenbieb <plantenbieb@outlook.com>
2020-09-14 11:16:09 +0200

    map alpha 0.2

--- js/bieb.js
+++ js/bieb.js
@@ -29,25 +29,5 @@ storeLocator({
         address: 'BESCHRIJVING',
         location: {lat: 51.908885, lng: 4.350489}
       },
-      {
-        name: 'PAEZ PALMA',
-        address: 'Carrer de la Bosseria, 12, 07001 Palma, Illes Balears, Spain',
-        location: {lat: 39.5706846, lng: 2.651996899999972}
-      },
-      {
-        name: 'PAEZ TENERIFE',
-        address: 'Calle Ramón y Cajal, 37, 38004 Santa Cruz de Tenerife, Spain',
-        location: {lat: 28.4660137, lng: -16.258979700000054}
-      },
-      {
-        name: 'PAEZ CHIADO',
-        address: 'R. do Alecrim 62, 1200-243 Lisboa, Portugal',
-        location: {lat: 38.7094783, lng: -9.143068699999958}
-      },
-      {
-        name: 'PAEZ PORTO',
-        address: 'R. das Carmelitas 151, 4050-162 Porto, Portugal',
-        location: {lat: 41.1462871, lng: -8.61489800000004}
-      }
     ]
   })
