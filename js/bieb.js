ffe85ec952224650e0afa6267a38ae3bbb74e7ec
plantenbieb <plantenbieb@outlook.com>
2020-09-15 12:43:26 +0200

    clean

---
+++ js/bieb.js
@@ -0,0 +1,36 @@
+storeLocator({
+    container: 'my-store-locator',
+    apiKey: 'AIzaSyA55LXw2hfXdWtViSc28ncD2MXjToagmJw',
+    markerIconSize: [40, 40],
+    limit: 1,
+    center: {lat: 51.9074, lng: 4.4930},
+    zoom: 11,
+    searchHint: "Kies een plantenbieb uit!",
+    homeLocationHint: 'Jouw locatie',
+    fullWidthMap: 'true',
+    stores: [
+      {
+        name: 'Plantenbieb.com',
+        address: 'Zet je plantenbieb nu op de kaart!',
+        location: {lat: 51.9074, lng: 4.4930},
+        website: 'https://plantenbieb.com'
+      },
+      {
+        name: 'NAAM',
+        address: "BESCHRIJVING",
+        location: {lat: 51.909101, lng: 4.446417}
+      },
+      {
+        name: "NAAM",
+        address: 'BESCHRIJVING',
+        location: {lat: 51.914326, lng: 4.400137}
+      },
+      {
+        name: 'NAAM',
+        address: 'BESCHRIJVING',
+        location: {lat: 51.908885, lng: 4.350489}
+      },
+    ]
+  })
+
+  
