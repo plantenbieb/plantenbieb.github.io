e160d2b03b520d0b7d2a0916b9f098c65b39ffa0
plantenbieb <plantenbieb@outlook.com>
2020-09-15 20:47:11 +0200

    0.2 met biebs

--- js/bieb.js
+++ js/bieb.js
@@ -18,52 +18,62 @@ storeLocator({
       {
         name: 'Happy Plants',
         address: "Stekjes kunnen geruild worden en bij adoptie van een plant vragen we een donatie.",
-        location: {lat: 51.909101, lng: 4.446417},
+        location: {lat: 51.592147, lng: 4.222144},
         website: 'https://www.instagram.com/happyplants_plantenasiel'
       },
       {
         name: 'Grave',
         address: "Kom kijken & ruilen bij ons.",
-        location: {lat: 51.909101, lng: 4.446417},
+        location: {lat: 51.761731, lng: 5.729051},
         website: 'https://www.instagram.com/plantenbiebgrave'
       },
       {
         name: 'Assen Marjon',
         address: "Twee vriendinnen met liefde voor planten (en elkaar) runnen een plantenbieb (met twee vestigingen) in Assen.",
-        location: {lat: 51.909101, lng: 4.446417},
+        location: {lat: 52.998342, lng: 6.506803},
         website: 'https://www.instagram.com/plantenbieb_assen'
       },
       {
         name: 'Assen Kirstin',
         address: "Twee vriendinnen met liefde voor planten (en elkaar) runnen een plantenbieb (met twee vestigingen) in Assen.",
-        location: {lat: 51.909101, lng: 4.446417},
+        location: {lat: 53.014162, lng: 6.592897},
         website: 'https://www.instagram.com/plantenbieb_assen'
       },
       {
         name: 'Breda',
         address: "Elkaar blij maken met planten. bloemen en zaden.",
-        location: {lat: 51.909101, lng: 4.446417},
-        website: 'https://www.instagram.com/happyplants_plantenasiel'
+        location: {lat: 51.615288, lng: 4.721949},
+        website: 'https://www.instagram.com/plantenbieb_breda'
       },
       {
         name: "Veendam",
         address: 'Trotste eigenaar van de eerste plantenbieb in Nederland!',
-        location: {lat: 51.914326, lng: 4.400137}
+        location: {lat: 53.104986, lng: 6.861491}
       },
       {
         name: 'Ten Boer',
         address: 'Je kunt je stekjes. zaden en planten ruilen of doneren.',
-        location: {lat: 51.908885, lng: 4.350489}
+        location: {lat: 53.273198, lng: 6.687785},
+        website: 'https://m.me/PlantenbiebTenBoer'
+      },
+      {
+        name: 'Natuurcentrum Gorinchem',
+        address: 'De plantenbieb van Mascotte Gijs is hier te vinden.',
+        location: {lat: 53.273198, lng: 6.687785},
+        website: 'https://m.me/PlantenbiebTenBoer'
       },
       {
         name: 'Plantenasiel Fonteinbos',
         address: 'Plantenbieb in Zoetermeer.',
-        location: {lat: 52.058974, lng: 4.478049}
+        location: {lat: 52.058974, lng: 4.478049},
+        website: 'https://www.facebook.com/PlantenasielZoetermeer'
       },
       {
         name: 'Flevoland',
         address: 'Plantenbieb in Zoetermeer.',
-        location: {lat: 52.051722, lng: 4.462061}
+        location: {lat: 52.051722, lng: 4.462061},
+        website: 'https://www.facebook.com/PlantenasielZoetermeer'
       },
     ]
   })
+  
