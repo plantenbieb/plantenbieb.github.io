3b5a0cfd6ed605665652925c1e5b828b1d6abf6e
plantenbieb <plantenbieb@outlook.com>
2020-09-18 14:55:50 +0200

    add kweker

--- js/kwekers.js
+++ js/kwekers.js
@@ -11,97 +11,97 @@ storeLocator({
     stores: [
       {
         name: 'Global Garden',
-        address: "Kamerplanten, potterie, sfeer, streek, kleding, dieren, bomen, buitenplanten en tuin.",
+        address: "Tuincentrum",
         location: {lat: 52.308811, lng: 4.609820},
         website: 'https://www.globalgarden.nl/'
       },
       {
         name: 'Groencentrum Sonneveld',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 52.076057, lng: 4.639961},
         website: 'https://www.groencentrumsonneveld.nl/'
       },
       {
         name: 'Sonneveld',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 51.672087, lng: 4.772042},
         website: 'https://www.groencentrumsonneveld.nl/'
       },
       {
         name: 'Life and Garden Renesse',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 51.739166, lng: 3.783799},
         website: 'https://www.lifeandgarden.com/vestiging/life-garden-renesse'
       },
       {
         name: 'Bloempje Sluis',
-        address: "Zierikzee",
+        address: "Tuincentrum",
         location: {lat: 51.648912, lng: 3.920236},
         website: 'https://www.bloempjezz.nl/'
       },
       {
         name: 'Bloempje Sluis',
-        address: "Nieuwerkerk",
+        address: "Tuincentrum",
         location: {lat: 51.464325, lng: 3.997925},
         website: ''
       },
       {
         name: 'Hanenburg',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 52.062027, lng: 4.244931},
         website: 'https://www.tuincentrum-hanenburg.nl/'
       },
       {
         name: 'Nieuw Hanenburg',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 52.062027, lng: 4.244931},
         website: 'https://www.tuincentrumnieuwhanenburg.nl/'
       },
       {
         name: 'Little Green shop',
-        address: "Bar & Zo little green shop",
+        address: "Winkel",
         location: {lat: 52.081369, lng: 4.291904},
         website: 'https://www.bar-enzo.com/'
       },
       {
         name: 'Hoveniers Veenslagen',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 52.304697, lng: 6.493408},
         website: 'https://www.tuincentrumveeneslagen.nl/'
       },
       {
         name: 'Myoso',
-        address: "In-store Shop",
+        address: "Winkel",
         location: {lat: 52.266451, lng: 6.794786},
         website: ''
       },
       {
         name: 'Welkoop Haaksbergen',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 52.172781, lng: 6.743319},
         website: 'https://www.welkoop.nl/winkels/haaksbergen'
       },
       {
         name: 'Rood en Bloem',
-        address: "",
+        address: "Winkel",
         location: {lat: 52.088841, lng: 5.118621},
         website: 'https://roodenbloem.nl/'
       },
       {
         name: 'Steck',
-        address: "",
+        address: "Winkel",
         location: {lat: 52.118336, lng: 5.133277},
         website: 'https://steckutrecht.nl/'
       },
       {
         name: 'De Carlton',
-        address: "",
+        address: "Tuincentrum",
         location: {lat: 51.988413, lng: 4.187932},
         website: 'https://www.decarlton.nl/vestiging/de-carlton-tuincentrum'
       },
       {
         name: 'Bloosom',
-        address: "In-store Shop",
+        address: "Winkel",
         location: {lat: 52.630554, lng: 4.748915},
         website: ''
       },
@@ -110,6 +110,90 @@ storeLocator({
         address: "Kwekerij",
         location: {lat: 51.927919, lng: 2.571101},
         website: 'https://www.debloemenplantage.nl/contact/'
+      },
+      {
+        name: 'De Bloementuin',
+        address: "Tuincentrum",
+        location: {lat: 52.696726, lng: 6.190587},
+        website: 'https://bloementuin-meppel.nl/contact/'
+      },
+      {
+        name: 'Aralia',
+        address: "Tuincentrum",
+        location: {lat: 51.844519, lng: 4.171496},
+        website: 'https://www.aralia.nl/'
+      },
+      {
+        name: 'Voorne',
+        address: "Tuincentrum",
+        location: {lat: 51.921103, lng: 4.125635},
+        website: 'http://tuincentrumvoorne.nl/'
+      },
+      {
+        name: 'Van der Dussen',
+        address: "Tuincentrum",
+        location: {lat: 51.981013, lng: 4.596225},
+        website: 'https://www.uwtuincentrum.nl/contact/'
+      },
+      {
+        name: 'Stek de Stadstuinwinkel',
+        address: "Winkel",
+        location: {lat: 51.913720, lng: 4.462811},
+        website: 'https://www.stekrotterdam.nl/'
+      },
+      {
+        name: 'Zwinkels',
+        address: "Tuincentrum",
+        location: {lat: 51.921916, lng: 4.551106},
+        website: 'http://www.tuincentrumzwinkels.nl/contact/contactgegevens/'
+      },
+      {
+        name: 'De Bloemenjungle',
+        address: "Bloemencentrum",
+        location: {lat: 51.858895, lng: 4.612049},
+        website: 'https://bloemenjungle.nl/bloemenjungle-over-ons-wie-zijn-wij/'
+      },
+      {
+        name: 'Bloemsierkunst van der Steeg',
+        address: "Winkel",
+        location: {lat: 000, lng: 000},
+        website: 'https://bloemsierkunstvdsteeg.nl/contact'
+      },
+      {
+        name: 'Groene Vingers',
+        address: "Winkel",
+        location: {lat: 52.011001, lng: 4.361137},
+        website: 'https://groenevingersdelft.nl/contact/route/'
+      },
+      {
+        name: 'Filya Indoor Garden',
+        address: "Winkel",
+        location: {lat: 52.079762, lng: 4.318234},
+        website: 'https://groenevingersdelft.nl/contact/'
+      },
+      {
+        name: 'Ockenburgh',
+        address: "Tuincentrum",
+        location: {lat: 52.052224, lng: 4.227599},
+        website: 'https://www.tuincentrumockenburgh.nl/'
+      },
+      {
+        name: 'Boers',
+        address: "Tuincentrum",
+        location: {lat: 51.953185, lng: 4.225396},
+        website: 'http://www.tuincentrumboersbv.nl/'
+      },
+      {
+        name: 'De Bosrand',
+        address: "Tuincentrum",
+        location: {lat: 52.139818, lng: 4.414390},
+        website: 'https://www.tcdebosrand.nl/vestiging/tuincentrum-de-bosrand-wassenaar'
+      },
+      {
+        name: 'Van der Spek',
+        address: "Kweker",
+        location: {lat: 000, lng: 000},
+        website: ''
       },
     ]
   })
