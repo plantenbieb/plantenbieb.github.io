0909e1c26fc5f2e9340c7c4d1483dfec72a1546b
plantenbieb <plantenbieb@outlook.com>
2020-09-18 15:22:59 +0200

    0.3

---
+++ js/biebs.js
@@ -0,0 +1,96 @@
+storeLocator({
+    container: 'Plantenbiebs',
+    apiKey: 'AIzaSyA55LXw2hfXdWtViSc28ncD2MXjToagmJw',
+    markerIconSize: [40, 40],
+    limit: 1,
+    center: {lat: 51.9074, lng: 4.4930},
+    zoom: 11,
+    searchHint: "Kies een plantenbieb uit!",
+    homeLocationHint: 'Jouw locatie',
+    fullWidthMap: 'true',
+    stores: [
+      {
+        name: 'Happy Plants',
+        address: "Stekjes kunnen geruild worden en bij adoptie van een plant vragen we een donatie.",
+        location: {lat: 51.592147, lng: 4.222144},
+        website: 'https://www.instagram.com/happyplants_plantenasiel'
+      },
+      {
+        name: 'Grave',
+        address: "Kom kijken & ruilen bij ons.",
+        location: {lat: 51.761731, lng: 5.729051},
+        website: 'https://www.instagram.com/plantenbiebgrave'
+      },
+      {
+        name: 'Assen Marjon',
+        address: "Twee vriendinnen met liefde voor planten (en elkaar) runnen een plantenbieb (met twee vestigingen) in Assen.",
+        location: {lat: 52.998342, lng: 6.506803},
+        website: 'https://www.instagram.com/plantenbieb_assen'
+      },
+      {
+        name: 'Assen Kirstin',
+        address: "Twee vriendinnen met liefde voor planten (en elkaar) runnen een plantenbieb (met twee vestigingen) in Assen.",
+        location: {lat: 53.014162, lng: 6.592897},
+        website: 'https://www.instagram.com/plantenbieb_assen'
+      },
+      {
+        name: 'Breda',
+        address: "Elkaar blij maken met planten. bloemen en zaden.",
+        location: {lat: 51.615288, lng: 4.721949},
+        website: 'https://www.instagram.com/plantenbieb_breda'
+      },
+      {
+        name: "Veendam",
+        address: 'Trotste eigenaar van de eerste plantenbieb in Nederland!',
+        location: {lat: 53.104986, lng: 6.861491}
+      },
+      {
+        name: 'Ten Boer',
+        address: 'Je kunt je stekjes. zaden en planten ruilen of doneren.',
+        location: {lat: 53.273198, lng: 6.687785},
+        website: 'https://m.me/PlantenbiebTenBoer'
+      },
+      {
+        name: 'Natuurcentrum Gorinchem',
+        address: 'De plantenbieb van Mascotte Gijs is hier te vinden.',
+        location: {lat: 51.839637, lng: 4.954197},
+        website: 'https://natuurcentrumgorinchem.nl/ontdek-het-eiland'
+      },
+      {
+        name: 'Plantenasiel Fonteinbos',
+        address: 'Plantenbieb in Zoetermeer.',
+        location: {lat: 52.058974, lng: 4.478049},
+        website: 'https://www.facebook.com/PlantenasielZoetermeer'
+      },
+      {
+        name: 'Flevoland',
+        address: 'Plantenbieb in Zoetermeer.',
+        location: {lat: 52.051722, lng: 4.462061},
+        website: 'https://www.facebook.com/PlantenasielZoetermeer'
+      },
+      {
+        name: 'Plantenbiebje',
+        address: 'Doneer of ruil en geniet.  Max 3 plantjes/stekjes p.p.',
+        location: {lat: 51.578880, lng: 4.655301},
+        website: 'https://www.instagram.com/plantenbiebje/'
+      },
+      {
+        name: 'PlantZoen',
+        address: 'Een kruidenbak om kruiden te knippen en veel stekjes die geruild kunnen worden!',
+        location: {lat: 51.623976, lng: 4.940472},
+        website: 'https://www.instagram.com/plantzoen.dongen/'
+      },
+      {
+        name: 's Gravendeel',
+        address: 'Een grote bieb met veel verschillend aanbod van stekjes, plantjes, zaden en potten.',
+        location: {lat: 51.776213, lng: 4.614081},
+        website: 'https://www.instagram.com/plantenbieb.sgravendeel'
+      },
+      {
+        name: 'Plantenasiel Alkmaar',
+        address: "",
+        location: {lat: 52.642406, lng: 4.74293},
+        website: 'https://www.plantenasielalkmaar.nl/'
+      },
+    ]
+  })
