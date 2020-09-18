878bd7247e67b0ac0c043add1e5122cb13cd5392
plantenbieb <plantenbieb@outlook.com>
2020-09-18 16:01:03 +0200

    0.3

---
+++ js/kwekers.js
@@ -0,0 +1,19 @@
+storeLocator({
+    container: 'my-store-locator',
+    apiKey: 'AIzaSyA55LXw2hfXdWtViSc28ncD2MXjToagmJw',
+    markerIconSize: [40, 40],
+    limit: 1,
+    center: {lat: 51.9074, lng: 4.4930},
+    zoom: 11,
+    searchHint: "Zoek een kweker of tuincentrum uit!",
+    homeLocationHint: 'Jouw locatie',
+    fullWidthMap: 'true',
+    stores: [
+      {
+        name: 'Global Garden',
+        address: "Kamerplanten, potterie, sfeer, streek, kleding, dieren, bomen, buitenplanten en tuin.",
+        location: {lat: 52.308811, lng: 4.609820},
+        website: 'https://www.globalgarden.nl/'
+      },
+    ]
+  })
